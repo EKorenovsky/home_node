8ee4c06e58c62510e5f4ee830c61f116b0331c01
EKorenovsky <ekorenovsky@gmail.com>
2020-02-08 01:26:09 +0300

    5 омашка

---
+++ home5/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+
+const apiV1 = require('./routes/apiV1');
+
+let options = {
+    dotfiles: 'ignore',
+    maxAge: "1d",
+    etag: false
+}
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static('public', options));
+app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
+app.use('', apiV1);
+
+
+app.listen(8080);
---
+++ home5/routes/apiV1.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const genGUID = require('uuid/v1');
+const router = express.Router();
+
+
+function getKey() {
+    return genGUID();
+}
+
+
+
+router.post('/apiV1', function(req, res, next) {
+
+    if (req.body.method == 'get_key')
+        return res.status("200").send({ status: "success", message: "key was successfully create", key: getKey(), ip: req.connection.remoteAddress });
+
+    if (req.body.method == 'send_lead')
+        return res.status("200").send({ status: "success", message: "lead was successfully sent", data: { name: req.body.params.name, email: req.body.params.email, phone: req.body.params.phone }, key: req.body.key, ip: req.connection.remoteAddress });
+
+    return res.status("404").send({ "status": "error", "message": "no method find", "ip": req.connection.remoteAddress });
+
+
+});
+
+
+
+
+
+
+module.exports = router;
